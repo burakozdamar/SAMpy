b09d61154c661cf6aa45c4ce4040aece18089624
burakozdamar <burak.ozdamar@gmail.com>
2020-12-31 11:16:22 +0100

    added some funcs

--- test/open_file.py
+++ test/open_file.py
@@ -1,4 +1,4 @@
-#!/bin/envs/python3
+#!/usr/bin/env/python3
 
 import sys
 import numpy as np
@@ -8,7 +8,8 @@ sys.path.append('..')
 from utils.xyz_utils import read_xyz
 
 ff = "PEG8_traj2_pos70to130ps-10steps.xyz"
-mass = {'H': 1.00794,'C':12.0107,'O':15.9994,'Si':28.0855,'Cl':35.4527,'K':39.0983,'Al':26.981539}
+mass = {'H': 1.00794,'C1':1, 'C':12.0107, 'O':15.9994,'Si':28.0855,'Cl':35.4527,'K':39.0983,'Al':26.981539}
+pbc_ = np.array([13.386,13.286,85])#*1044).reshape(-1,3)
 
 def memoize_mass():
   print("memo")
@@ -21,22 +22,40 @@ def memoize_mass():
 
 mass_arr = memoize_mass() 
 
+def translate(arr1,arr2):
+  return arr1-arr2
+
 def calcul_CM(xyz):
   coords = xyz.coords
-  
-  x_sum = np.sum(coords[:,0] * mass_arr)
-  y_sum = np.sum(coords[:,1] * mass_arr)
-  z_sum = np.sum(coords[:,2] * mass_arr)
-
-  print(x_sum,y_sum,z_sum) 
-  if x_sum/np.sum(mass_arr) > 13.386/2:
-      print("here", x_sum/np.sum(mass_arr) -  13.386/2)
-  print(x_sum/np.sum(mass_arr))
-  print(y_sum/np.sum(mass_arr))
-  print(z_sum/np.sum(mass_arr))
+  com = np.average(coords, axis=0, weights=mass_arr)
+  t = translate(coords, com)
+  print("coords", coords)
+  print("com", com)
+  print("t",t)
+  pbc(t)
+  #print(com)
+  #print(coords - com)
 
 def pbc(arr):
-  pass
+  print(arr[0,0])
+  if arr[0,0] < -pbc_[0]/2:
+      arr[0,0] += pbc_[0]
+  if arr[0,0] > pbc_[0]/2:
+      arr[0,0] -= pbc_[0]
+
+  if arr[0,1] < -pbc_[1]/2:
+      arr[0,1] += pbc_[1]
+  if arr[0,1] > pbc_[1]/2:
+      arr[0,1] -= pbc_[1]
+
+  if arr[0,2] < -pbc_[2]/2:
+      arr[0,2] += pbc_[2]
+  if arr[0,2] > pbc_[2]/2:
+      arr[0,2] -= pbc_[2]
+
+  print(arr[0])
+  #print(arr[arr < -pbc_/2][:5])
+  #print(pbc_/2)
 
 def open_file(ff):
 

bc363c8f3cf8eb674d4264473b52bc2c50ec2d37
burakozdamar <burak.ozdamar@gmail.com>
2021-01-01 10:02:45 +0100

    baby steps

--- test/open_file.py
+++ test/open_file.py
@@ -6,38 +6,49 @@ import numpy as np
 sys.path.append('..')
 
 from utils.xyz_utils import read_xyz
+from collections import Counter 
 
-ff = "PEG8_traj2_pos70to130ps-10steps.xyz"
-mass = {'H': 1.00794,'C1':1, 'C':12.0107, 'O':15.9994,'Si':28.0855,'Cl':35.4527,'K':39.0983,'Al':26.981539}
-pbc_ = np.array([13.386,13.286,85])#*1044).reshape(-1,3)
+ff = "../warehouse/PEG.xyz"
+#ff = "../warehouse/test.xyz"
 
-def memoize_mass():
+mass = {'H': 1.00794,'C':0,'x':12.0107,'O':15.9994,'Si':28.0855,'Cl':35.4527,'K':39.0983,'Al':26.981539}
+pbc_ = np.array([13.386,13.286,85])
+
+def memoize_mass(ff):
   print("memo")
   with open(ff) as f:
     xyz = read_xyz(f)
     atomtypes = xyz.atomtypes
+    #for a in atomtypes:
+    #  if a == "C":
+        
     mass_arr = [mass[atom] for atom in atomtypes]
     mass_arr = np.array(mass_arr)
   return mass_arr
 
-mass_arr = memoize_mass() 
+mass_arr = memoize_mass(ff) 
 
 def translate(arr1,arr2):
   return arr1-arr2
 
 def calcul_CM(xyz):
   coords = xyz.coords
-  com = np.average(coords, axis=0, weights=mass_arr)
+  atomtypes = xyz.atomtypes
+  print(Counter(atomtypes))
+  print(atomtypes)
+  print(mass_arr, sum(mass_arr))
+  com = np.true_divide(mass_arr.sum(0),(mass_arr!=0).sum(0))
+  #com = np.average(coords, axis=0, weights=mass_arr)
   t = translate(coords, com)
-  print("coords", coords)
+  #print("coords", coords)
   print("com", com)
-  print("t",t)
+  #print("t",t)
   pbc(t)
   #print(com)
   #print(coords - com)
 
 def pbc(arr):
-  print(arr[0,0])
+  print("before:", arr[0])
   if arr[0,0] < -pbc_[0]/2:
       arr[0,0] += pbc_[0]
   if arr[0,0] > pbc_[0]/2:
@@ -53,14 +64,15 @@ def pbc(arr):
   if arr[0,2] > pbc_[2]/2:
       arr[0,2] -= pbc_[2]
 
-  print(arr[0])
+  print("after",arr[0])
+
   #print(arr[arr < -pbc_/2][:5])
   #print(pbc_/2)
 
 def open_file(ff):
 
   with open(ff) as f:
-    for i in range(100):
+    for i in range(2):
       try:
         xyz = read_xyz(f)
         print(f"Finished reading {i}")

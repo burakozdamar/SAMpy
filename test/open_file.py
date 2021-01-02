984f4f7d445374ef4b55975fb6d8a85fdfdddfbd
burakozdamar <burak.ozdamar@gmail.com>
2021-01-02 13:42:27 +0100

    write_xyz works

--- test/open_file.py
+++ test/open_file.py
@@ -6,16 +6,21 @@ import numpy as np
 sys.path.append('..')
 
 ff = sys.argv[1]
+gg = "pos_recentered_py.xyz"
 
 from utils.xyz_utils import read_xyz
-from collections import Counter 
-
-#ff = "../warehouse/mini.xyz"
-#ff = "../warehouse/PEG.xyz"
-#ff = "../warehouse/test.xyz"
+from collections import Counter
+from itertools import cycle 
 
 mass = {'H': 1.00794,'x':0,'C':12.0107,'O':15.9994,'Si':28.0855,'Cl':35.4527,'K':39.0983,'Al':26.981539}
 pbc_ = np.array([13.386,13.286,85])
+
+
+def write_xyz(fout, coords, title="", atomtypes=("A",)):
+    with open(gg,'a') as fout:
+      fout.write("%d\n%s\n" % (coords.size / 3, title))
+      for x, atomtype in zip(coords.reshape(-1, 3), cycle(atomtypes)):
+          fout.write("%s %.8g %.8g %.8g\n" % (atomtype, x[0], x[1], x[2]))
 
 def memoize_mass(ff):
   mass_arr = []
@@ -25,17 +30,9 @@ def memoize_mass(ff):
   # silicons or alums coming later from C
   if 'C' in atomtypes:
     mass_arr = np.array([mass[atom] if atom == 'Si' else 0 for atom in atomtypes])
-    
-  print(Counter(atomtypes))
+  return mass_arr, atomtypes
 
-  #mass_arr = np.array([mass[atom] for atom in atomtypes if atom!="C" else 0])
-  #mass_arr = np.array([mass[atom] for atom in atomtypes])
-  print("orig", mass_arr)
-  #mass_arr = mass_arr[np.nonzero(mass_arr)]
-  print("modif", mass_arr)
-  return mass_arr
-
-mass_arr = memoize_mass(ff) 
+mass_arr, atomtypes = memoize_mass(ff)
 
 def translate(arr1,arr2):
   return arr1-arr2
@@ -44,56 +41,38 @@ def calcul_CM(xyz):
   coords = xyz.coords
   atomtypes = xyz.atomtypes
   com = np.average(coords, axis=0, weights=mass_arr)
-  t = translate(coords, com)
-  #print("com", com)
-  #print('t',t)
-  t_prime = pbc(t)
-  #pbc1(t)
-  print("t'",t_prime)
-  #return t
+  t = pbc(translate(coords, com))
+  return t
 
 def pbc(arr):
-  arr = np.where(arr < (-pbc_/2), arr+(pbc_), arr)
-  arr = np.where(arr > (pbc_/2), arr-(pbc_), arr)
+  arr = np.where(arr < -pbc_/2, arr+pbc_, arr)
+  arr = np.where(arr >  pbc_/2, arr-pbc_, arr)
   return arr 
 
+def write_header(g):
+  ret = len(mass_arr)
+  #print(ret)
+  g.write(str.encode(str(ret)+'\n\n'))
 
-def pbc1(arr):
-
-  print("before:", arr[0])
-  if arr[0,0] < -pbc_[0]/2:
-      arr[0,0] += pbc_[0]
-  if arr[0,0] > pbc_[0]/2:
-      arr[0,0] -= pbc_[0]
-
-  if arr[0,1] < -pbc_[1]/2:
-      arr[0,1] += pbc_[1]
-  if arr[0,1] > pbc_[1]/2:
-      arr[0,1] -= pbc_[1]
-
-  if arr[0,2] < -pbc_[2]/2:
-      arr[0,2] += pbc_[2]
-  if arr[0,2] > pbc_[2]/2:
-      arr[0,2] -= pbc_[2]
-
-  print("after",arr[0])
-  #print(arr[arr < -pbc_/2][:5])
-  #print(pbc_/2)
-
-def write_to_file():
-  pass
+def write_to_file(g,arr):
+  write_header(g)
+  np.savetxt(g, arr, fmt='%.6f')
 
 def open_file(ff):
 
   with open(ff) as f:
-    for i in range(2):
-      try:
-        xyz = read_xyz(f)
-        print(f"{i}")
-      except:
-        print("DONE")
-        break
+    with open(gg,'w') as g:
+      for i in range(20):
+        try:
+          xyz = read_xyz(f)
+          print(f"{i}")
+          t = calcul_CM(xyz)
+          write_xyz(g, t, title="", atomtypes=atomtypes) 
+          #write_to_file(g,t)
+        except Exception as e:
+          print("DONE",e)
+          break
   
-      calcul_CM(xyz)
-
 open_file(ff)
+
+ 

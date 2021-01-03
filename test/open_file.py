244ff537ddf2b92064f8be110baae1ea07b4397d
burakozdamar <burak.ozdamar@gmail.com>
2021-01-03 19:21:43 +0100

    buggy but works

--- test/open_file.py
+++ test/open_file.py
@@ -8,35 +8,77 @@ sys.path.append('..')
 
 ff = sys.argv[1]
 gg = "py_pos_recentered.xyz"
+hh = "py_pos_rebuilt.xyz"
 
 from utils.xyz_utils import read_xyz
-from collections import Counter,namedtuple
+from collections import Counter, namedtuple
 from itertools import cycle, count, groupby
 
 mass = {'H': 1.00794,'x':0,'C':12.0107,'O':15.9994,'Si':28.0855,'Cl':35.4527,'K':39.0983,'Al':26.981539}
 pbc_ = np.array([13.386,13.286,85])
 
+def memoize_mass(xyz):
+  print('here')
+  print(xyz.atomtypes)
+  mass_arr = np.array([mass[atom] for atom in xyz.atomtypes])  
+  return mass_arr 
+ # if kind=='water':
+ #   mass_arr = np.array([mass[atom] for atom in atomtypes])
+ # else:
+ #   if 'C' in atomtypes:
+ #     mass_arr = np.array([mass[atom] if atom == 'Si' else 0 for atom in atomtypes])
+ #   else:
+ #     mass_arr = np.array([mass[atom] for atom in atomtypes])
+ # return mass_arr, atomtypes
 
-def memoize_mass(ff):
-  mass_arr = []
-  with open(ff) as f:
-    xyz = read_xyz(f)
-    atomtypes = xyz.atomtypes
-  # silicons or alums coming later from C
-  if 'C' in atomtypes:
-    mass_arr = np.array([mass[atom] if atom == 'Si' else 0 for atom in atomtypes])
-  return mass_arr, atomtypes
-
-mass_arr, atomtypes = memoize_mass(ff)
+#mass_arr, atomtypes = memoize_mass(ff)
 
 def translate(arr1,arr2):
   return arr1-arr2
 
-def calcul_CM(xyz):
+def calcul_CM(xyz, kind='Si'):
+  
+  if kind=='water':
+    mass_arr = np.array([mass[atom] for atom in xyz.atomtypes])
+    coords = xyz.coords
+    com = np.average(coords, axis=0, weights=mass_arr[:coords.shape[0]])
+    
+
+  mass_arr = memoize_mass(xyz)
   coords = xyz.coords
-  com = np.average(coords, axis=0, weights=mass_arr)
+  com = np.average(coords, axis=0, weights=mass_arr[:coords.shape[0]])
+
+  mass_arr = np.array([mass[atom] if atom == 'Si' else 0 for atom in xyz.atomtypes])  
   t = pbc(translate(coords, com))
   return t
+
+def water_molecules(xyz):
+  data = xyz.data
+  step = data[0]
+  atomtypes = xyz.atomtypes
+  coords = xyz.coords
+  data_dct = read_boxdata()
+  val = data_dct['$NO']
+  coords = coords[:int(val[0])*3]  
+  atomtypes = atomtypes[:int(val[0])*3]  
+  return namedtuple("waters", ["coords", "step", "atomtypes"])(
+        coords, step, atomtypes 
+    )
+
+#def read_xyz(fin):
+#    natoms = int(fin.readline())
+#    step = int(fin.readline()[:-1].split()[-1])
+#    coords = np.zeros([natoms, 3], dtype="float64")
+#    atomtypes = []
+#    for x in coords:
+#        line = fin.readline().split()
+#        atomtypes.append(line[0])
+#        x[:] = list(map(float, line[1:4]))
+#
+#    return namedtuple("XYZFile", ["coords", "step", "atomtypes"])(
+#        coords, step, atomtypes
+#    )
+
 
 def pbc(arr):
   arr = np.where(arr < -pbc_/2, arr+pbc_, arr)
@@ -58,20 +100,37 @@ def read_boxdata():
     v = [[i.split(' ') if len(i.split(' '))>1 else i for i in sl] for sl in v]
     return dict(zip(k,v))
 
+BOXDATA = read_boxdata()
+trans_val = float(BOXDATA['$ZTRASL'][0].replace('d','.'))
+
+
 def open_file(ff):
 
-  with open(ff) as f:
-    with open(gg,'w') as g:
-      for i in count(1):
-        try:
-          xyz = read_xyz(f)
-          if i%1000==0: print(f"{i}")
-          t = calcul_CM(xyz)
-          write_xyz(g, t, title="", atomtypes=atomtypes) 
-          #write_to_file(g,t)
-        except ValueError as e:
-          print("DONE")
-          break
-  
+  f= open(ff)
+  g= open(gg,'w')
+  h= open(hh,'w')
+  #j= open(jj,'w')
+  #mass_arr, atomtypes = memoize_mass(f)   
+  for i in count(1):
+    try:
+      xyz = read_xyz(f)
+ 
+    except ValueError as e:
+      print("DONE")
+      break
+
+    if i%1==0: print(f"x{i}")
+    t = calcul_CM(xyz, kind='Si')
+    water_mols = water_molecules(xyz)
+    tp = calcul_CM(water_mols)
+    print(water_mols)
+    #t_trans = translate(water_molecules(xyz),-trans_val)
+    t_rebuilt = calcul_CM(water_mols)
+    write_xyz(g, t, title="", atomtypes=xyz.atomtypes) 
+    write_xyz(h, water_mols.coords, atomtypes=xyz.atomtypes) 
+     
+  f.close()
+  g.close()
+  h.close()
+
 open_file(ff)
-read_boxdata()
--- utils/xyz_utils.py
+++ utils/xyz_utils.py
@@ -12,7 +12,7 @@ def read_xyz(f):
     energy = float(data[8].split(",")[0])
 
     data = np.array([step, time, energy])
-    coords = np.zeros([natoms, 3], dtype="float64")
+    coords= np.zeros([natoms, 3], dtype="float64")
     atomtypes = []
 
     for x in coords:
@@ -23,3 +23,19 @@ def read_xyz(f):
     return namedtuple("XYZFile", ["coords", "data", "atomtypes"])(
         coords, data, atomtypes
     )
+
+
+def hhread_xyz(fin):
+    natoms = int(fin.readline())
+    step = int(fin.readline()[:-1].split()[-1])
+    coords = np.zeros([natoms, 3], dtype="float64")
+    atomtypes = []
+    for x in coords:
+        line = fin.readline().split()
+        atomtypes.append(line[0])
+        x[:] = list(map(float, line[1:4]))
+
+    return namedtuple("XYZFile", ["coords", "step", "atomtypes"])(
+        coords, step, atomtypes
+    )
+

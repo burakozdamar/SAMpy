740870a5e62ed36fd9f682801f543a3eb3358453
burakozdamar <burak.ozdamar@gmail.com>
2021-01-03 15:27:52 +0100

    i can read BOXDATA

--- test/open_file.py
+++ test/open_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env/python3
 
+import re
 import sys
 import numpy as np
 
@@ -10,7 +11,7 @@ gg = "py_pos_recentered.xyz"
 
 from utils.xyz_utils import read_xyz
 from collections import Counter,namedtuple
-from itertools import cycle ,count
+from itertools import cycle, count, groupby
 
 mass = {'H': 1.00794,'x':0,'C':12.0107,'O':15.9994,'Si':28.0855,'Cl':35.4527,'K':39.0983,'Al':26.981539}
 pbc_ = np.array([13.386,13.286,85])
@@ -48,6 +49,15 @@ def write_xyz(fout, coords, title="", atomtypes=("A",)):
     #fout.write("%s %.8g %.8g %.8g\n" % (atomtype, x[0], x[1], x[2]))
     fout.write('{:2s} {:>12.6f} {:>12.6f} {:>12.6f}\n'.format(atomtype, x[0], x[1], x[2]))
 
+def read_boxdata():
+  with open("../warehouse/BOXDATA") as f:
+    ll = [l.strip() for l in f.readlines()]
+    res = [list(sub) for ele, sub in groupby(ll, key = bool) if ele] 
+    k = [item[0] for item in res]
+    v = [item[1:] for item in res]
+    v = [[i.split(' ') if len(i.split(' '))>1 else i for i in sl] for sl in v]
+    return dict(zip(k,v))
+
 def open_file(ff):
 
   with open(ff) as f:
@@ -64,3 +74,4 @@ def open_file(ff):
           break
   
 open_file(ff)
+read_boxdata()

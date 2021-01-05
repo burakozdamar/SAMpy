00f509f4f53f9439f01e3a507d32c5c63c44df09
burakozdamar <burak.ozdamar@gmail.com>
2021-01-05 23:54:42 +0100

    Recenter done, interface on the way

--- test/interface.py
+++ test/interface.py
@@ -13,12 +13,15 @@ jj = "py_pos_rebuilt_solid.xyz"
 from utils.xyz_utils import read_xyz, read_boxdata
 from collections import Counter, namedtuple
 from itertools import cycle, count, groupby
+from profiler.prof import profile_me
 
 mass = {'H': 1.00794,'C':12.0107,'O':15.9994,'Si':28.0855,'Cl':35.4527,'K':39.0983,'Al':26.981539}
-pbc_ = np.array([13.386,13.286,85])
 
 BOXDATA = read_boxdata()
+pbc_ = np.array([13.386,13.286,85])
 trans_val = abs(float(BOXDATA['$ZTRASL'][0].replace('d','.')))
+num_oxygens = int(BOXDATA['$NO'][0])
+num_waters = int(BOXDATA['$NO'][0])*3
 
 def memoize_mass(ff):
   print("MEMO")
@@ -40,81 +43,29 @@ def sep(xyz):
 
   com = np.average(coords, axis=0, weights=mass_arr[:])
   translated = translate(coords, com)
-  translated = pbc(translated)
+  translated = apply_pbc(translated)
   translated[:,2] -= trans_val
 
-  waters = translated[:n_wat]
-  solids = translated[n_wat:]
+  waters = translated[:num_waters]
+  solids = translated[num_waters:]
 
-  waters = pbc(waters)
+  waters = apply_pbc(waters)
   waters = rebuild_water(waters)
   
-  solids = pbc(solids) 
+  solids = apply_pbc(solids) 
   final_tr = np.concatenate((waters, solids), axis=0) 
 
   return namedtuple("sepnt", ["coords", "data", "atomtypes"])(
         final_tr, data, atomtypes 
     )
 
-
-def pbc(arr):
+def apply_pbc(arr):
   arr1 = np.where(arr < -pbc_/2, arr+pbc_, arr)
   arr = np.where(arr1 >  pbc_/2, arr-pbc_, arr1)
   return arr 
 
 def translate(arr1,arr2):
   return arr1-arr2
-
-def translate_to_CM(xyz, kind='Si', linear_trans=0):
-  data = xyz.data
-  coords = xyz.coords
-  atomtypes = xyz.atomtypes
-  n_oxygens = BOXDATA['$NO']
-  solids = coords[int(n_oxygens[0])*3:]
-
-  if 'C' in xyz.atomtypes:
-    mass_arr = np.array([mass[atom] if atom == kind else 0 for atom in xyz.atomtypes])  
-  else:
-    mass_arr = np.array([mass[atom] for atom in xyz.atomtypes])
-
-  com = np.average(coords, axis=0, weights=mass_arr[:coords.shape[0]])
-  translated = translate(coords, com)
-
-  #water_mols = water_molecules(translated, translate=trans_val, rebuilt=True)
-  solids -= linear_trans
-  
-  t_solids = pbc(solids)
-
-  #final_tr = np.concatenate((water_mols.coords, t_solids), axis=0) 
-  final_tr = 0
-  #translated = pbc(translated)
-
-  return namedtuple("trans_to_CM", ["coords", "data", "atomtypes"])(
-        final_tr, data, atomtypes 
-    )
-  #return translated
-
-def water_molecules(xyz, translate=0, rebuilt=False):
-  data = xyz.data
-  atomtypes = xyz.atomtypes
-  coords = xyz.coords
-  n_oxygens = BOXDATA['$NO']
-  coords = coords[:int(n_oxygens[0])*3]  
-  atomtypes = atomtypes[:int(n_oxygens[0])*3]  
-  coords[:,2] -= translate
-
-  if rebuilt:
-    #print("rebuilt", coords[0])
-    coords = pbc(coords)
-    #print(coords[0])
-    #coords = water_pbc(coords)
-    #coords = test_pbc(coords)
-    #print(coords[0])
-  #print(coords.shape)
-
-  return namedtuple("waters", ["coords", "data", "atomtypes"])(
-        coords, data, atomtypes 
-    )
 
 def rebuild_water(t):
 
@@ -131,11 +82,26 @@ def rebuild_water(t):
 
   return t
 
+#format is slow!
 def write_xyz(fout, coords, title="", atomtypes=("A",)):
   fout.write("%d\n%s\n" % (coords.size / 3, title))
   for x, atomtype in zip(coords.reshape(-1, 3), cycle(atomtypes)):
     fout.write('{:2s} {:>12.6f} {:>12.6f} {:>12.6f}\n'.format(atomtype, x[0], x[1], x[2]))
+    #fout.write("%s %.18g %.18g %.18g\n" % (atomtype, x[0], x[1], x[2]))
 
+def grid():
+  E = 2.4
+  d = np.array([0.5, 0.5, 0.25])
+  n = np.array(list(map(round, (pbc_/d)[:])))
+  f = - (pbc_ - d)/2 
+  return (d, n, f, (n-1)*d+f)
+
+ # r = np.sqrt(xdiff**2+ydiff**2+zdiff**2)
+ # if (r<=3*E):
+ #   p = p + exp(-r**2/(2*E**2))/((2*np.pi*E**2)**1.5)
+
+
+@profile_me
 def main(ff, boundary=1):
 
   f = open(ff)
@@ -150,14 +116,11 @@ def main(ff, boundary=1):
       print("DONE")
       break
 
-    #if i%1==0: print(f"x{i}")
-    print(i)
     sep1 = sep(xyz)
-    #t = translate_to_CM(xyz, kind='Si', linear_trans = trans_val)
-    #water_mols = water_molecules(t, translate=trans_val, rebuilt=True)
     
     step_num = int(sep1.data[0])
-    #write_xyz(g, t.coords, title='x', atomtypes=xyz.atomtypes) 
+    #interface.xyz to write
+    #grid_interface to write
     write_xyz(h, sep1.coords[:360], title=f"step wat = {step_num}", atomtypes=xyz.atomtypes) 
     write_xyz(j, sep1.coords , title=f"step rebu = {i}", atomtypes=xyz.atomtypes) 
      
@@ -166,4 +129,7 @@ def main(ff, boundary=1):
   j.close()
 
 mass_arr = memoize_mass(ff) 
-main(ff)
+
+grid()
+#main(ff)
+#print(read_boxdata())
